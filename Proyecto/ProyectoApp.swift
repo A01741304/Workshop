450ccb6099822e5010a3c6e779e31948859e85e6
Vale <valerialaguilarlmeza@hotmail.com>
2023-12-05 17:08:22 -0600

    Initial Commit

---
+++ Proyecto/ProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoApp.swift
+//  Proyecto
+//
+//  Created by Gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
